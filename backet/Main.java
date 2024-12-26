8f536dd06c2dfbcfd533d271aa08c61d5145a457
zov-pixel <71325623+zov-pixel@users.noreply.github.com>
2024-12-26 16:09:57 +0300

    Create Main.java

---
+++ backet/Main.java
@@ -0,0 +1,10 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Basket basket = new Basket();
+        basket.add("Milk", 200, 2, 200);
+        basket.print("Milk");
+    }
+}
