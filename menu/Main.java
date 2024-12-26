32f09e61323ac77e16d53afa6dcc23353b67e805
zov-pixel <71325623+zov-pixel@users.noreply.github.com>
2024-12-26 18:55:03 +0300

    Create Main.java

---
+++ menu/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        DinnerMenu dinnerMenu = new DinnerMenu();
+        System.out.println(dinnerMenu.canMake(1000, 400, 5,
+            10, 30, 8));
+    }
+
+}
