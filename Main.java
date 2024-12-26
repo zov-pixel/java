92a55055a6593a424c003c6c1ab7dd4d57025d77
zov-pixel <71325623+zov-pixel@users.noreply.github.com>
2024-12-26 15:03:20 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
-package ru.lessons.lesson3.task3;
-
+package ru.lessons.lesson2.task1;
 
 public class Main {
 
 
+
     public static void main(String[] args) {
-        SwimmingPool swimmingPool = new SwimmingPool();
-        System.out.println(swimmingPool.getPoolFillingTime(1200, 30, 10));
+        DinnerMenu dinnerMenu = new DinnerMenu();
+        System.out.println(dinnerMenu.canMake(1000, 400, 5,
+            10, 30, 8));
     }
+
 }
