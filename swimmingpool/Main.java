d717dc9fbf0aa34f6aa7381b7287c638719fd828
zov-pixel <71325623+zov-pixel@users.noreply.github.com>
2024-12-26 19:04:39 +0300

    Create Main.java

---
+++ swimmingpool/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        SwimmingPool swimmingPool = new SwimmingPool();
+        System.out.println(swimmingPool.getPoolFillingTime(1200, 30, 10));
+    }
+}
